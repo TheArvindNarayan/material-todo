71c7c8c511375ab5e214255b1af980d9208b1496
Arvind Narayan <TheArvindNarayan>
2017-09-02 17:49:48 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+$( ".toggle" ).click(function() {
+    $(this).removeClass("fa-play");
+    $(this).addClass("fa-stop");
+
+});
+
+
+$( ".fa-stop" ).click(function() {
+    $(this).removeClass("fa-stop");
+    $(this).addClass("fa-stop");
+
+});
